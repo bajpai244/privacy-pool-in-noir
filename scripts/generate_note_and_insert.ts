18e99dd9a6fc2846c4abeaf832de45271e3cc540
Harsh Bajpai <bajpaiharsh244@gmail.com>
2025-07-09 15:04:46 +0400

    feat: working script for note generation and insertion

--- scripts/generate_note_and_insert.ts
+++ scripts/generate_note_and_insert.ts
@@ -15,8 +15,14 @@ const main = async () => {
   const { storage, tree } = await getTreeAndStorage();
 
   const note = generateNote(100);
+  await storage.setNote(note);
 
-  storage.setItem("note", note);
+  console.log("tree root before insert", tree.root);
+  console.log("Inserting note into tree");
+  tree.insert(note.commitment);
+  console.log("tree root after insert", tree.root);
+
+  console.log("inserted note into tree: ", note);
 };
 
 main();
